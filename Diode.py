8922a652960db828445dd1abe56c146378578525
Paulo Machado <79323978+paulibobo@users.noreply.github.com>
2024-12-11 11:53:06 +0000

    Update Diode.py

--- Diode.py
+++ Diode.py
@@ -19,6 +19,7 @@ parser.add_argument('-reservoir_size', type=int, default=3500)
 parser.add_argument('-sparsity', type=float, default=0.5)
 parser.add_argument('-savepath', type=str, default="res/Optimized.dat")
 parser.add_argument('-plots', type=bool, default=True)
+parser.add_argument('-train', type=bool, default=False)
 opt = parser.parse_args()
 
 inter = opt.interpolations+1 #interpolation amount
@@ -27,208 +28,206 @@ r_size = opt.reservoir_size
 spars = opt.sparsity
 datafile_path = opt.savepath
 plot = opt.plots
+train = opt.train
 
-
-length=201 #datapoints per training example
-dataArrayx = [] 
-dataArrayy = [] 
-
-files= 0
-for filename in os.listdir("L_06/Train"): #Read training data
-        fname = "L_06/Train/" + filename
-        with open(fname) as f:
-            files+=1
-            tempArrayx = [] 
-            tempArrayy = [] 
-            for line in f: 
-                    temp = [float(x) for x in line.split()]
-                    xvals =[temp[0],temp[1]]
-                    yvals = [temp[2]]
-                    tempArrayx.append(xvals)
-                    tempArrayy.append(yvals)
-            dataArrayx.append(tempArrayx)
-            dataArrayy.append(tempArrayy)
-        
-
-
-dataArrayx = np.array(dataArrayx)
-dataArrayy = np.array(dataArrayy)
-
-
-
+if(train == True):
+    length=201 #datapoints per training example
+    dataArrayx = [] 
+    dataArrayy = [] 
     
-newtrainy = [] #Interpolate new values
-newtrainx = []
-for i in range(0,files):
-    interp_func1 = interp1d(dataArrayx[i,:,0] , dataArrayy[i,:,0],bounds_error=False,  fill_value="extrapolate" )
-    interp_func3 = interp1d(dataArrayx[i,:,0] , dataArrayx[i,:,1],bounds_error=False,  fill_value="extrapolate" )
-    temparr = []
-    temparrx = []
-    for l in range(0,len(dataArrayy[i])):
-        newarr = []
-        newarr3 = []
-        temptrainx = []
-        if(l+1<len(dataArrayy[i])):
-            if(30<l<50 or 160<l<180):   #Interpolate only in the relevant areas
-                newarr = interp_func1(np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0]))) 
-                newarr3 = interp_func3(np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0]))) 
-                temptrainx= np.vstack((np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0])), newarr3)).T
+    files= 0
+    for filename in os.listdir("Train"): #Read training data
+            fname = "Train/" + filename
+            with open(fname) as f:
+                files+=1
+                tempArrayx = [] 
+                tempArrayy = [] 
+                for line in f: 
+                        temp = [float(x) for x in line.split()]
+                        xvals =[temp[0],temp[1]]
+                        yvals = [temp[2]]
+                        tempArrayx.append(xvals)
+                        tempArrayy.append(yvals)
+                dataArrayx.append(tempArrayx)
+                dataArrayy.append(tempArrayy)
+            
+    
+    
+    dataArrayx = np.array(dataArrayx)
+    dataArrayy = np.array(dataArrayy)
+    
+    
+    
+        
+    newtrainy = [] #Interpolate new values
+    newtrainx = []
+    for i in range(0,files):
+        interp_func1 = interp1d(dataArrayx[i,:,0] , dataArrayy[i,:,0],bounds_error=False,  fill_value="extrapolate" )
+        interp_func3 = interp1d(dataArrayx[i,:,0] , dataArrayx[i,:,1],bounds_error=False,  fill_value="extrapolate" )
+        temparr = []
+        temparrx = []
+        for l in range(0,len(dataArrayy[i])):
+            newarr = []
+            newarr3 = []
+            temptrainx = []
+            if(l+1<len(dataArrayy[i])):
+                if(30<l<50 or 160<l<180):   #Interpolate only in the relevant areas
+                    newarr = interp_func1(np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0]))) 
+                    newarr3 = interp_func3(np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0]))) 
+                    temptrainx= np.vstack((np.arange(dataArrayx[i,l,0], dataArrayx[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx[0,1,0] - dataArrayx[0,0,0])), newarr3)).T
+                else:
+                    newarr.append(dataArrayy[i,l,0])
+                    newarr3.append(dataArrayx[i,l,1])
+                    temptrainx= np.vstack((dataArrayx[i,l,0], newarr3)).T
             else:
                 newarr.append(dataArrayy[i,l,0])
                 newarr3.append(dataArrayx[i,l,1])
                 temptrainx= np.vstack((dataArrayx[i,l,0], newarr3)).T
-        else:
-            newarr.append(dataArrayy[i,l,0])
-            newarr3.append(dataArrayx[i,l,1])
-            temptrainx= np.vstack((dataArrayx[i,l,0], newarr3)).T
-
-        for k in range(0,len(newarr)):
-            temparr.append([newarr[k]])
-            temparrx.append([temptrainx[k,0],temptrainx[k,1]])
-    newtrainy.append(temparr)
-    newtrainx.append(temparrx)
-
-plt.figure(0) #Plot figure and compute MSE
-
-
-
-
-
-dataArrayx2 = [] 
-dataArrayy2 = [] 
-
-files= 0
-for filename in os.listdir("L_06/Test"): #Read testing data
-        fname = "L_06/Test/" + filename
-        with open(fname) as f:
-            files+=1
-            tempArrayx = [] 
-            tempArrayy = [] 
-            for line in f: # read rest of lines
-                    temp = [float(x) for x in line.split()]
-                    xvals =[temp[0],temp[1]]
-                    yvals = [temp[2]]
-                    tempArrayx.append(xvals)
-                    tempArrayy.append(yvals)
-            dataArrayx2.append(tempArrayx)
-            dataArrayy2.append(tempArrayy)
-        
-
-
-dataArrayx2 = np.array(dataArrayx2)
-dataArrayy2 = np.array(dataArrayy2)
-
-
-
-
     
-
-
-newtesty = [] #Interpolate new values
-newtestx = []
-for i in range(0,files):
-    interp_func1 = interp1d(dataArrayx2[i,0:length,0] , dataArrayy2[i,0:length,0],bounds_error=False,  fill_value="extrapolate" )
-    interp_func3 = interp1d(dataArrayx2[i,0:length,0] , dataArrayx2[i,0:length,1],bounds_error=False,  fill_value="extrapolate" )
-    temparr = []
-    temparrx = []
-    for l in range(0,len(dataArrayy[i])):
-        newarr = []
-        newarr3 = []
-        temptrainx = []
-        if(l+1<len(dataArrayy[i])):
-            if(30<l<50 or 160<l<180):   #Interpolate only in the relevant areas
-                newarr = interp_func1(np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0])))     
-                newarr3 = interp_func3(np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0]))) 
-                temptrainx= np.vstack((np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0])), newarr3)).T
+            for k in range(0,len(newarr)):
+                temparr.append([newarr[k]])
+                temparrx.append([temptrainx[k,0],temptrainx[k,1]])
+        newtrainy.append(temparr)
+        newtrainx.append(temparrx)
+    
+    plt.figure(0) #Plot figure and compute MSE
+    
+    
+    
+    
+    
+    dataArrayx2 = [] 
+    dataArrayy2 = [] 
+    
+    files= 0
+    for filename in os.listdir("Test"): #Read testing data
+            fname = "Test/" + filename
+            with open(fname) as f:
+                files+=1
+                tempArrayx = [] 
+                tempArrayy = [] 
+                for line in f: # read rest of lines
+                        temp = [float(x) for x in line.split()]
+                        xvals =[temp[0],temp[1]]
+                        yvals = [temp[2]]
+                        tempArrayx.append(xvals)
+                        tempArrayy.append(yvals)
+                dataArrayx2.append(tempArrayx)
+                dataArrayy2.append(tempArrayy)
+            
+    
+    
+    dataArrayx2 = np.array(dataArrayx2)
+    dataArrayy2 = np.array(dataArrayy2)
+    
+    
+    
+    
+        
+    
+    
+    newtesty = [] #Interpolate new values
+    newtestx = []
+    for i in range(0,files):
+        interp_func1 = interp1d(dataArrayx2[i,0:length,0] , dataArrayy2[i,0:length,0],bounds_error=False,  fill_value="extrapolate" )
+        interp_func3 = interp1d(dataArrayx2[i,0:length,0] , dataArrayx2[i,0:length,1],bounds_error=False,  fill_value="extrapolate" )
+        temparr = []
+        temparrx = []
+        for l in range(0,len(dataArrayy[i])):
+            newarr = []
+            newarr3 = []
+            temptrainx = []
+            if(l+1<len(dataArrayy[i])):
+                if(30<l<50 or 160<l<180):   #Interpolate only in the relevant areas
+                    newarr = interp_func1(np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0])))     
+                    newarr3 = interp_func3(np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0]))) 
+                    temptrainx= np.vstack((np.arange(dataArrayx2[i,l,0], dataArrayx2[i,l+1,0]-0.0000000000000001,(1/inter)*(dataArrayx2[0,1,0] - dataArrayx2[0,0,0])), newarr3)).T
+                else:
+                    newarr.append(dataArrayy2[i,l,0])
+                    newarr3.append(dataArrayx2[i,l,1])
+                    temptrainx= np.vstack((dataArrayx2[i,l,0], newarr3)).T
             else:
                 newarr.append(dataArrayy2[i,l,0])
                 newarr3.append(dataArrayx2[i,l,1])
                 temptrainx= np.vstack((dataArrayx2[i,l,0], newarr3)).T
-        else:
-            newarr.append(dataArrayy2[i,l,0])
-            newarr3.append(dataArrayx2[i,l,1])
-            temptrainx= np.vstack((dataArrayx2[i,l,0], newarr3)).T
-
-        for k in range(0,len(newarr)):
-            temparr.append([newarr[k]])
-            temparrx.append([temptrainx[k,0],temptrainx[k,1]])
-    newtesty.append(temparr)
-    newtestx.append(temparrx)
-
-
-plt.plot(np.array(newtestx[0])[:,0],np.array(newtesty[0])[:,0], color='red')
-plt.legend(["ESN prediction","Ground truth"], loc ="lower left") 
- 
-k=0
-
-
-initArrx = []
-initArry = []
-for i in range(0,len(newtrainx)): #Generate initial point model data
-    initArrx.append(newtrainx[i][0:10])
-    initArry.append(newtrainy[i][0:10])
     
-initArrx = np.array(initArrx)
-initArry = np.array(initArry)
-
-
-reg = ESNRegressor(spectral_radius = 0.99, sparsity = spars, n_reservoir= r_size, hidden_size = 1000, feedback = True) #Main model
-reg2 = ESNRegressor(spectral_radius = 0.99, sparsity = spars, n_reservoir= 200, hidden_size = 1000, feedback = True) #Initial point model
-
-start = time.time()
-
-
-
-for k in range(0,epochs):
-    print("Epoch: " + str(k+1))
-    for i in range(0,len(newtrainy)): #train main model
-        xdata = np.array(newtrainx[i][:])
-        ydata= np.array(newtrainy[i])[:]
-        reg.fit(X=xdata, y=ydata)
-
-
-for k in range(0,epochs):  
-    for i in range(0,len(initArrx)): #train initial point model
-        reg2.fit(X=initArrx[i,:], y=initArry[i,:])
-
-
-print(ESNRegressor)
-end = time.time()
+            for k in range(0,len(newarr)):
+                temparr.append([newarr[k]])
+                temparrx.append([temptrainx[k,0],temptrainx[k,1]])
+        newtesty.append(temparr)
+        newtestx.append(temparrx)
     
-
-
-y_pred = reg.predict(np.array(newtestx[0][:]))  #Make predictions
-
-y_pred2 = reg2.predict(np.array(newtestx[0][0:30]))
-
-for i in range(0,30): #Replace initial point preeditions
-        y_pred[i]=y_pred2[i]
-
-
-#Plot figure and compute MSE
-
-mse = mean_squared_error(np.array(newtesty[0])[:,0],y_pred[:]) 
-print("MSE for dataset "": " + str(mse) + " Training time:" + str(end - start) + " seconds")
-
-
-fileprint = []
-
-k=0
-for i in y_pred:
-    num = y_pred[k]
-    num2= num[0]
-    fileprint.append([np.array(newtestx[0])[k,0],num2])
-    k+=1
-
-#print(fileprint)
-np.savetxt(datafile_path , fileprint, fmt=['%10.7f','%10.7f'])
+    
+    plt.plot(np.array(newtestx[0])[:,0],np.array(newtesty[0])[:,0], color='red')
+    plt.legend(["ESN prediction","Ground truth"], loc ="lower left") 
+     
+    k=0
+    
+    
+    initArrx = []
+    initArry = []
+    for i in range(0,len(newtrainx)): #Generate initial point model data
+        initArrx.append(newtrainx[i][0:10])
+        initArry.append(newtrainy[i][0:10])
+        
+    initArrx = np.array(initArrx)
+    initArry = np.array(initArry)
+    
+    
+    reg = ESNRegressor(spectral_radius = 0.99, sparsity = spars, n_reservoir= r_size, hidden_size = 1000, feedback = True) #Main model
+    reg2 = ESNRegressor(spectral_radius = 0.99, sparsity = spars, n_reservoir= 200, hidden_size = 1000, feedback = True) #Initial point model
+    
+    start = time.time()
+    
+    
+    
+    for k in range(0,epochs):
+        print("Epoch: " + str(k+1))
+        for i in range(0,len(newtrainy)): #train main model
+            xdata = np.array(newtrainx[i][:])
+            ydata= np.array(newtrainy[i])[:]
+            reg.fit(X=xdata, y=ydata)
+    
+    
+    for k in range(0,epochs):  
+        for i in range(0,len(initArrx)): #train initial point model
+            reg2.fit(X=initArrx[i,:], y=initArry[i,:])
+    
+    
+    print(ESNRegressor)
+    end = time.time()
+        
+    
+    
+    y_pred = reg.predict(np.array(newtestx[0][:]))  #Make predictions
+    
+    y_pred2 = reg2.predict(np.array(newtestx[0][0:30]))
+    
+    for i in range(0,30): #Replace initial point preeditions
+            y_pred[i]=y_pred2[i]
+    
+    
+    #Plot figure and compute MSE
+    
+    mse = mean_squared_error(np.array(newtesty[0])[:,0],y_pred[:]) 
+    print("MSE for dataset "": " + str(mse) + " Training time:" + str(end - start) + " seconds")
+    
+    
+    fileprint = []
+    
+    k=0
+    for i in y_pred:
+        num = y_pred[k]
+        num2= num[0]
+        fileprint.append([np.array(newtestx[0])[k,0],num2])
+        k+=1
+    
+    #print(fileprint)
+    np.savetxt(datafile_path , fileprint, fmt=['%10.7f','%10.7f'])
         
 
 
 if(plot == True):
-    #plt.plot(np.array(newtestx[0])[:,0],y_pred[:], color = 'blue' ) 
-    #plt.plot(np.array(newtestx[0])[:,0],np.array(newtesty[0])[:,0], color='red')
-    #plt.legend(["ESN prediction","Ground truth"], loc ="upper center") 
     
     plotArray = []
     fname = "res/true.dat"
@@ -294,8 +293,7 @@ if(plot == True):
     xESNo = plotArray[:,0]   # position [um]
     rhoESNo = plotArray[:,1] #electron density [um^(-3)]
     
-    print(rhot)
-    
+
     plt.figure(0)
     plt.plot(xt,rhot, linewidth=2, color = (1,0,0))
     plt.plot(xGRU,rhoGRU, linestyle = 'dashed', linewidth = 2, color= (0,0.7,0))
